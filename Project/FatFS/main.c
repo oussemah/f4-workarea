6a291670720e5e9ec4a8a5fbada6578bc9e124ed
Torrentula <orwar@web.de>
2013-10-21 16:34:53 +0200

    removed unneeded code and added more serial debug output

--- Project/FatFS/main.c
+++ Project/FatFS/main.c
@@ -91,84 +91,8 @@ void USART_puts(USART_TypeDef* USARTx, volatile char *s){
 	}
 }
 
-void USART_putn(USART_TypeDef* USARTx, unsigned char* data, uint16_t len){
-	while(len--){
-		USART_putc(USARTx, *data);
-		data++;
-	}
-}
-
-/* This funcion shows how to initialize
-* the GPIO pins on GPIOD and how to configure
-* them as inputs and outputs
-*/
-void init_GPIO(void){
-
-	/* This TypeDef is a structure defined in the
-	* ST's library and it contains all the properties
-	* the corresponding peripheral has, such as output mode,
-	* pullup / pulldown resistors etc.
-	*
-	* These structures are defined for every peripheral so
-	* every peripheral has it's own TypeDef. The good news is
-	* they always work the same so once you've got a hang
-	* of it you can initialize any peripheral.
-	*
-	* The properties of the periperals can be found in the corresponding
-	* header file e.g. stm32f4xx_gpio.h and the source file stm32f4xx_gpio.c
-	*/
-	GPIO_InitTypeDef GPIO_InitStruct;
-
-	/* This enables the peripheral clock to the GPIOD IO module
-	* Every peripheral's clock has to be enabled
-	*
-	* The STM32F4 Discovery's User Manual and the STM32F407VGT6's
-	* datasheet contain the information which peripheral clock has to be used.
-	*
-	* It is also mentioned at the beginning of the peripheral library's
-	* source file, e.g. stm32f4xx_gpio.c
-	*/
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-	/* In this block of instructions all the properties
-	* of the peripheral, the GPIO port in this case,
-	* are filled with actual information and then
-	* given to the Init function which takes care of
-	* the low level stuff (setting the correct bits in the
-	* peripheral's control register)
-	*
-	*
-	* The LEDs on the STM324F Discovery are connected to the
-	* pins PD12 thru PD15
-	*/
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_15 | GPIO_Pin_14 | GPIO_Pin_13 | GPIO_Pin_12; // we want to configure all LED GPIO pins
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT; // we want the pins to be an output
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz; // this sets the GPIO modules clock speed
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP; // this sets the pin type to push / pull (as opposed to open drain)
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL; // this sets the pullup / pulldown resistors to be inactive
-	GPIO_Init(GPIOD, &GPIO_InitStruct); // this finally passes all the values to the GPIO_Init function which takes care of setting the corresponding bits.
-
-	/* This enables the peripheral clock to
-	* the GPIOA IO module
-	*/
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-
-	/* Here the GPIOA module is initialized.
-	* We want to use PA0 as an input because
-	* the USER button on the board is connected
-	* between this pin and VCC.
-	*/
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;	// we want to configure PA0
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN; // we want it to be an input
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;//this sets the GPIO modules clock speed
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP; // this sets the pin type to push / pull (as opposed to open drain)
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN; // this enables the pulldown resistor --> we want to detect a high level
-	GPIO_Init(GPIOA, &GPIO_InitStruct);	// this passes the configuration to the Init function which takes care of the low level stuff
-}
-
 int main(void)
 {
-	init_GPIO();
 	init_USART1(9600);
 	
 	FATFS FatFs;
@@ -177,22 +101,35 @@ int main(void)
 	
 	unsigned char buffer[6];
 
-
+	USART_puts(USART1, "Mounting volume...");
 	res = f_mount(&FatFs, "", 1); // mount the drive
-	res = f_open(&fil, "hello.txt", FA_OPEN_EXISTING | FA_WRITE | FA_READ); // open existing file in read and write mode
+	if (res)
+	{
+		USART_puts(USART1, "error occured!\n");
+		while(1);
+	}
+	USART_puts(USART1, "success!\n");
 	
-	//f_lseek(&fil, 0); // set rw pointer to top of file
+	USART_puts(USART1, "Opening file: \"hello.txt\"...");
+	res = f_open(&fil, "hello.txt", FA_OPEN_EXISTING | FA_WRITE | FA_READ); // open existing file in read and write mode
+	if (res)
+	{
+		USART_puts(USART1, "error occured!\n");
+		while(1);
+	}
+	USART_puts(USART1, "success!\n");
+	
 	f_gets(buffer, 6, &fil);
 	
-	//f_lseek(&fil, 6); // set rw pointer to pos 6
+	USART_puts(USART1, "I read: \"");
+	USART_puts(USART1, buffer);
+	USART_puts(USART1, "\"\n");
+	
 	f_puts(buffer, &fil);
 	
 	f_close(&fil); // close the file
 	f_mount(NULL, "", 1); // unmount the drive
 	
-	
-	uint8_t i = 0; 
-
 	while (1){  
 
 	}
